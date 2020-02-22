580658954dcaa14fd38bcfbef7c3f6e5d1695d21
manuelhzr <manuel_hzr@outlook.com>
2020-02-21 19:37:18 -0500

    The World Bank

---
+++ The World Bank/src/Clases/CLIENTE.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author Asus
+ */
+public class CLIENTE {
+    private int Idcliente;
+    private String Nombre;
+    private String Direccion;
+    private int Telefono;
+    private String Correo;
+            
+    public CLIENTE() {            
+    }
+
+    public int getIdcliente() {
+        return Idcliente;
+    }
+
+    public void setIdcliente(int Idcliente) {
+        this.Idcliente = Idcliente;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String Direccion) {
+        this.Direccion = Direccion;
+    }
+
+    public int getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(int Telefono) {
+        this.Telefono = Telefono;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String Correo) {
+        this.Correo = Correo;
+    }
+    
+    
+}
